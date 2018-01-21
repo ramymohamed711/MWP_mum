be10962b7c62b4b812690b2917bf2ffe7eff5f07
RamyMohamedSaad <ramy711@yahoo.com>
2018-01-21 23:46:47 +0200

    Lab5 exercises upated

--- Lab5/exercise/routes/users.js
+++ Lab5/exercise/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fetch = require('node-fetch')
 var router = express.Router();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const obj = fetch('http://jsonplaceholder.typicode.com/users/')
+  .then(function(res) {
+      return res.json();
+  }).then(function(body) {
+      res.render('users',{ users: body })
+  });
 });
 
 module.exports = router;
